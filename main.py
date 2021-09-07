e5a20e1c539bc989fcf30b606d2a52f2e9400b82
Corne1401 <47864642+Corne1401@users.noreply.github.com>
2021-09-07 14:05:54 -0600

    Lectura de tableros

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
+
+def brute_force():
+    print(Tablero)
+    return
+
+def main(tdNum):
+    #Variable de tablero global para poder utilizarla en otros metodos
+    global Tablero
+    Tablero = []
+
+    #Abre el archivo y lo lee, si no lo encuentra devuelve un mensaje y cierra el programa
+    try:
+        with open('Table_gen/TableroDoble'+str(tdNum)+'.txt') as tdFile:
+            lines = tdFile.readlines()
+            for i in range(len(lines)):
+                lines[i] = lines[i].replace("\n","")
+    except:
+        print('No se encontro el tablero')
+        return
+
+    #inserta los valores del tablero del archivo en una matriz para poder utilizarla posteriormente 
+    for line in lines[2:]:
+        if line == '':
+            break
+        Tablero.append(line.split(" ")[:-1])
+    
+    return
+
+
 if __name__ == "__main__":
-    print("Hello world")
+    main(1)
+    brute_force()
