a383c717d5a374cfedc86d4c198e39bbc373f45b
max <maximoandreaq@gmail.com>
2021-09-23 01:16:13 -0600

    Backtracking casi listo
    Falta probarlo e incorporarlo en un  solo main.

--- Algorithms/Backtracking.py
+++ Algorithms/Backtracking.py
@@ -8,10 +8,9 @@ from Helpers.misc import swap, verify
 # @param pTiles = Lista de fichas que sera utilizada para verificaciones
 # @param pSolution = Posible solucion a verificar
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-listaFichas = []
-listaIndices = []
-listaSoluciones = [] #Aqui se colocaran las distintas combinaciones ya probadas, para poder hacer la poda
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+listaFichas     = []
+listaIndices    = []
 
 #Genera una lista con listas que representan cada ficha ej: [[0,0],[0,1],[1,1]]
 def generarDominos(n):
@@ -23,10 +22,10 @@ def generarDominos(n):
     return lista
 
 def generarTabla(n):
-	global listaIndices
-	total = (n-1)*(n+2)
+    global listaIndices
+    total = (n-1)*(n+2)
     for i in range(0,total):
-    	listaIndices += [False]
+        listaIndices += [False]
     return listaIndices
 
 # Convierte a el int de solucion(sol) del mismo largo que el de prefijo(pre)
@@ -42,30 +41,63 @@ def reductor(sol,pre):
 
 #Sol representa la solucion que se le esta pasando
 #Mientras que pre, es el prefijo de la lista que no funciono
-def comparador(sol,pre):
+def compViejo(sol,pre):
     sol = reductor(sol,pre)
     if (sol == pre):
         return True
     return False
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+solActual   = [] #Aqui se almacenaran los valores que han sido leidos de la solucion que se este leyendo
+listaSol    = [] #Aqui se colocaran las distintas combinaciones ya probadas, para poder hacer la poda
+
+#Recibe 2 listas, la solucion que se va a leer y el prefijo de la lista
+#de soluciones que fallaron
+def comparador(sol,pre):
+    value = True
+    for i in range(0,len(pre)):
+        if pre[i]!=sol[i]:
+            return False
+    return value
+
+#Pasa un int a lista
+def intToList(num):
+    if (type(num)==int):
+        lista = []
+        lista = [int(i) for i in str(num)]
+        return lista
+
+#Cuano el algoritmo haya escrito la solucion que lee y esta falla, la mete en la lista de soluciones malas
+def agregarSol():
+    global solActual
+    global listaSol
+    if (solActual.size() != 0)
+        listaSol.append(solActual)
+        solActual = []
+
 
 def backtracking(pBoard,pTiles, pSolution):
-	missingTiles = []
+    global listaSol
+    global solActual
 
-	# Si ya no hay fichas ni pasos en la solucion significa que todas fueron asignadas
+    # @var tile_holder = variable para guardar las fichas a buscar
+    tile_holder = [] 
+    
+    # Si ya no hay fichas ni pasos en la solucion significa que todas fueron asignadas
     # Pero si quedan fichas y ya no hay pasos significa que no fue una solucion valida
+
     if pTiles == [] and pSolution == []:
         return True
     elif(pTiles!=[] and pSolution == []):
         return False
-
     else:
+        # Se usan 2 for para recorrer la matriz de izquierda a derecha, de arriba a abajo
         for i in range(len(pBoard)):
             for j in range(len(pBoard[i])):
-
-
-
-            	# Se pregunta si lo que se esta leyendo en la matriz es un entero
+                
+                # Se pregunta si lo que se esta leyendo en la matriz es un entero
                 if(isinstance(pBoard[i][j], int)):
 
                     # Si la posicion 0 de la solucion es un 0, esto significa que se va a leer una ficha de forma Horizontal
@@ -78,10 +110,53 @@ def backtracking(pBoard,pTiles, pSolution):
                             # Las coordenadas leidas se convierten en string para marcar que ya se leyeron
                             pBoard[i][j] = str(pBoard[i][j])
                             pBoard[i][j+1] = str(pBoard[i][j+1])
+
+                            # Agrega la orientacion de la ficha a la lista de solucion actual
+                            solActual.append(0)
                         
                         #Si existe un error de indice esto significa que la solucion no es valida
                         except IndexError:
+                            solActual.append(0)
+                            agregarSol()
                             return False
+
+                    # Si la posicion 0 de la solucion es un 1, esto significa que se va a leer una ficha de forma Vertical
+                    elif(pSolution[0] == 1):
+                        try:
+                            #Se agrega la coordenada leida y la de abajo inmediata a tile_holder
+                            tile_holder.append(pBoard[i][j])
+                            tile_holder.append(pBoard[i+1][j])
+
+                            # Las coordenadas leidas se convierten en string para marcar que ya se leyeron
+                            pBoard[i][j] = str(pBoard[i][j])
+                            pBoard[i+1][j] = str(pBoard[i+1][j])
+
+                            # Agrega la orientacion de la ficha a la lista de solucion actual
+                            solActual.append(1)
+
+                        #Si existe un error de indice esto significa que la solucion no es valida    
+                        except IndexError:
+                            solActual.append(1)
+                            agregarSol()
+                            return False
+
+                    # Se verifica si la pieza de tile_holder existe en la lista de piezas 
+                    if(verify(tile_holder, pTiles)):
+
+                        # Se hace el intento de remover la pieza de la lista donde se busca, si falla se le da la vuelta y se vuelve a intentar
+                        try:
+                            pTiles.remove(tile_holder)
+    
+                        except:
+                            swap(tile_holder)
+                            pTiles.remove(tile_holder)
+                        
+                        # Se hace la llamada recursiva eliminando el paso actual de la solucion
+                        return brute_force(pBoard, pTiles, pSolution[1:])
+                    
+                    #Si no existe en la lista se asume que ya se coloco por lo tanto la solucion no es valida
+                    else:
+                        return False
 
 
 
--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from Helpers.misc import binary_increment, read_file, solucion_gen
 from Table_gen.dominoes import make_tiles, place_tile
 from Algorithms.BruteForce import *
 from copy import deepcopy
-
-
-
+from Algorithms.Backtracking import *
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -30,6 +28,30 @@ def main():
         binary_increment(posible_sol)
         
     print(sol_list)
+
+def mainBack():
+    N = 1
+    Tiles = make_tiles(N)
+    Solution_Size = len(Tiles)
+    posible_sol = solucion_gen(Solution_Size)
+    sol_list = []
+
+    read_file(Board, N)
+
+    for i in range(2**Solution_Size):
+        workBoard = deepcopy(Board)
+        workTiles = deepcopy(Tiles)
+
+        for i in listaSol:
+            if (comparador(posible_sol,listaSol[i])==False):        
+                if(brute_force(workBoard, workTiles, posible_sol)):
+                    sol_list.append(deepcopy(posible_sol))
+                    print(posible_sol)
+        
+        binary_increment(posible_sol)
+        
+    print(sol_list)
+
 
 if __name__ == "__main__":
     #@var Board = variable global donde se guarda la matriz a tablero a trabajar
